cce7768ea9917e62c0b3c422b4bd3a1e411ea9af
redkorr <redkor910.97@gmail.com>
2023-03-15 20:21:40 +0100

    created ThankYouState component

--- src/components/Form/components/ThankYouState/ThankYouState.tsx
+++ src/components/Form/components/ThankYouState/ThankYouState.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
+import styles from './ThankYouState.module.css';
+
+import Icon from '../../../../assets/icon-complete.svg';
+
 const ThankYouState = () => {
-  return <div></div>;
+  const handleClick = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className={styles.contentWrapper}>
+      <div className={styles.icon}>
+        <img src={Icon} />
+      </div>
+      <div className={styles.header}>
+        <p>THANK YOU!</p>
+      </div>
+      <div className={styles.text}>We've added your card details</div>
+      <div>
+        <button
+          className={styles.button}
+          onClick={handleClick}
+        >
+          Continue
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ThankYouState;
